7d5c63db857996322b33b9968d963a19bb34e0de
akshatksharma <akshatksharma@wustl.edu>
2020-05-07 02:02:50 -0500

    Updates

---
+++ precache-manifest.78af66d8e8ce1fc10514f7095a0e6ec2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1729e985cc4996e03f7b",
+    "url": "/09-todo-react-akshatksharma/static/js/runtime~main.1729e985.js"
+  },
+  {
+    "revision": "613b9bd5ebe2f1240008",
+    "url": "/09-todo-react-akshatksharma/static/js/main.613b9bd5.chunk.js"
+  },
+  {
+    "revision": "b5cc840def37e6af71ee",
+    "url": "/09-todo-react-akshatksharma/static/js/1.b5cc840d.chunk.js"
+  },
+  {
+    "revision": "613b9bd5ebe2f1240008",
+    "url": "/09-todo-react-akshatksharma/static/css/main.f686c4e3.chunk.css"
+  },
+  {
+    "revision": "4a1389794d83334f4db2e05755fab22f",
+    "url": "/09-todo-react-akshatksharma/index.html"
+  }
+];
